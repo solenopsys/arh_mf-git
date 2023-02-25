45dfe6288fb025f32f076fd2f9cbf3380d6e5f68
AlexStorm <info@alexstorm.ru>
2023-02-25 20:25:46 +0530

    rename dirs

--- webpack.config.js
+++ webpack.config.js
@@ -104,7 +104,7 @@ module.exports = {
     }),
     iconPlug(
       'packages/fui/icons/src',
-      'dist/modules/alexstorm/git',
+      'dist/modules/richteri/git',
       'packages/modules/git/src/assets/menu.json'
     ),
     sharedMappings.getPlugin(),
