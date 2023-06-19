18b4cd1dc4c864422a8aee04c6a81ef081a0cf48
AlexStorm <info@alexstorm.ru>
2023-06-19 12:48:26 +0530

    refactor modules

--- src/app/entry.module.ts
+++ src/app/entry.module.ts
@@ -1,5 +1,26 @@
 import {NgModule} from '@angular/core';
-import {IMPORTS_CONF, PROVIDERS_CONF} from "./app.module";
+import {BrowserModule} from '@angular/platform-browser';
+
+import {RouterModule, Routes} from '@angular/router';
+import {TABLE_PAGE} from "@solenopsys/ui-templates";
+import {FormsModule} from "@angular/forms";
+import {TABLES} from "./tables.config";
+
+const routes: Routes = [
+  TABLE_PAGE(':table'),
+];
+
+export const PROVIDERS_CONF = [
+  {provide: 'tables', useValue: TABLES},
+  {provide: 'assets_dir', useValue: "/fm/modules/mf-git"},
+  {provide: 'mod_name', useValue: "git"}
+]
+
+export const IMPORTS_CONF = [
+  BrowserModule,
+  RouterModule.forChild(routes),
+  FormsModule,
+]
 
 @NgModule({
   declarations: [],
